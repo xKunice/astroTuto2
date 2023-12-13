9754685c748355686431e52a51fed0cfbce756cb
kunice <kunice@protonmail.com>
2023-12-13 14:21:33 -0600

    se añade la ruta correcta de la pagina de netlify  para el RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://kuniceastrotutorial.netlify.app/"
+    site: "https://heartfelt-sable-d59ee5.netlify.app/"
 });
