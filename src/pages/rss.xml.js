c8e767aae6e776511c96615fff7f612d8ab56194
kunice <kunice@protonmail.com>
2023-12-14 08:06:38 -0600

    .

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,10 @@
 import rss from '@astrojs/rss';
-import { pagesGlobToRssItems } from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-
+  const posts = await getCollection("posts");
   return rss({
-    title: 'Aprendiz de Astro | Blog',
+    title: 'Andres Betancourt | Blog',
     description: 'Mi viaje aprendiendo Astro',
     site: context.site,
     items: posts.map((post) => ({
@@ -14,6 +13,6 @@ export async function GET(context) {
       description: post.data.description,
       link: `/posts/${post.slug}/`,
     })),
-    customData: `<language>en-es</language>`,
+    customData: `<language>en-us</language>`,
   })
 }
